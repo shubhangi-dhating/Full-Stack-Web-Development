f758fdf489268d54716aa0acf43ad76c82b0c96f
shubhangi-dhating <shubangidhating@gmail.com>
2026-04-07 22:55:39 +0530

    update

---
+++ Student Portfolio/scrip.js
@@ -0,0 +1,12 @@
+function showMessage() {
+  document.getElementById("msg").innerText =
+    "Thanks for visiting my portfolio!";
+}
+
+function toggleDarkMode() {
+  document.body.classList.toggle("dark");
+}
+
+function downloadResume() {
+  alert("Add your resume PDF link here");
+}
